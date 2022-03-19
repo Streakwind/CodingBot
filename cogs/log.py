f59c77f10422466c2223c549c84cf1d241f0f791
Streakwind <streakwindofficial@gmail.com>
2022-03-19 16:14:51 -0700

    yes

--- cogs/log.py
+++ cogs/log.py
@@ -51,7 +51,7 @@ class Log (commands.Cog):
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(config.guild_webhook, adapter=AsyncWebhookAdapter(session))
 
-            if message_after.guild.id == self.bot.bot_guild:
+            if ctx.author.id != self.bot_id:
 
                 embed = discord.Embed(title=f"{message_after.author} has edited a message!")
                 embed.add_field(name="TIME (UTC)", value=f"<t:{int(datetime.now().timestamp())}")
@@ -68,7 +68,7 @@ class Log (commands.Cog):
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(config.guild_webhook, adapter=AsyncWebhookAdapter(session))
 
-            if message.guild.id == self.bot.bot_guild:
+            if ctx.author.id != self.bot_id:
                 embed = discord.Embed(title=f"{message.author} has deleted a message!")
                 embed.add_field(name="TIME (UTC)", value=f"<t:{int(datetime.now().timestamp())}")
                 embed.add_field(name="MESSAGE", value=f"{message.content}")
